bd41a9adaa71875abca7cd415b998bf39810fc85
asimkatamale <asimkatamale@gmail.com>
2017-03-19 19:13:08 +0530

    Updated index.php
    A second developer has made changes

--- project/index.php
+++ project/index.php
@@ -1,3 +1,6 @@
 <?php 
 echo 'Through vim editor here';
 ?>
+In case another developer edits this file
+then we have to pull so that the changes made by other developer will be pulled to local working repository
+
